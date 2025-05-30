d69720394188ccdb1a22eabf42e4756861146041
Kajal Tiwari <kajaltiwari3002@gmail.com>
2025-05-30 22:56:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true// playerX, playerY
+let count = 0;
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+const resetGame = () => {
+    turnO = true;
+    count =0;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () =>{
+        if (turnO) {
+            //playerO
+            box.innerText = "O";
+            turnO = false;
+        } else {
+            //playerX
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+       //checkWinner();
+       count++;
+
+       let isWinner = checkWinner()
+       if(count === 9 && !iswinner){
+        gameDraw();
+       }
+
+    });
+
+});
+const gameDraw = () => {
+    msg.innerText = `Game was a Draw.`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+  };
+
+const disableBoxes =() =>{
+    for(let box of boxes){
+        box.disabled = true;
+    }
+};
+
+const enableBoxes =() =>{
+    for(let box of boxes){
+        box.enabled = false;
+        box.innerText = "";
+    }
+}
+const showWinner = (winner) =>{
+    msg.innerText=`Congrataulatin, Winner is Aditi  ${winner}`;
+    msgContainer.style.backgroundColor = "red"
+    
+    
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+
+checkWinner = () =>{
+    for(pattern of winPatterns){
+    //   console.log(pattern[0],pattern[1],pattern[2]);
+    //   console.log(boxes[pattern[0]].innerText,boxes[pattern[1]].innerText,boxes[pattern[2]].innerText);
+    //   console.log(
+    //     boxes[pattern[0]],innerText,
+    //     boxes[pattern[1]],innerText,
+    //     boxes[pattern[2]],innerText,
+    //   );
+      let pos1val = boxes[pattern[0]].innerText;
+      let pos2val = boxes[pattern[1]].innerText;
+      let pos3val = boxes[pattern[2]].innerText;
+
+      if(pos1val != "" && pos2val != "" && pos3val != ""){
+        if(pos1val === pos2val && pos2val === pos3val){
+            console.log("winner",pos1val);
+           
+            showWinner(pos1val);
+            return true;
+        }
+      }
+    } 
+};
+
+newGameBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
+
+
